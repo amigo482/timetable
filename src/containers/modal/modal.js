a158b98b8fc1908eef886bd06a9c8de40c9a1c00
Denis Ryabov <da-ryab0v@yandex.ru>
2018-10-23 14:51:29 +0300

    Refactoring and add close on escape function

--- src/containers/modal/modal.js
+++ src/containers/modal/modal.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CSSModules from 'react-css-modules';
-import {setCurrentModal, updateLibraryRequest, addLibraryRequest, addTimetable, updateTimetable, getTimes} from '../../actions/actions';
+import { setCurrentModal, updateLibraryRequest, addLibraryRequest, addTimetable, updateTimetable, getTimes } from '../../actions/actions';
 
 import styles from './modal.css';
 
@@ -12,43 +12,65 @@ import AddTimetableModal from './modals/addTimetable';
 import EditTimetableModal from './modals/editTimetable';
 
 class Modal extends Component {
+  componentDidMount() {
+    document.onkeydown=( event ) => {
+      event=event||window.event;
+      if ( event.keyCode==27 ) {
+        this.props.setCurrentModal( '' );
+      };
+    };
+  }
+
   render() {
-    const {modal, setCurrentModal, updateLibraryRequest, addLibraryRequest, current, libraries, faculty, addTimetable,  currentTimetable, updateTimetable, getTimes, times} = this.props;
-    switch(modal) {
+    const {
+      modal,
+      setCurrentModal,
+      updateLibraryRequest,
+      addLibraryRequest,
+      current,
+      libraries,
+      faculty,
+      addTimetable,
+      currentTimetable,
+      updateTimetable,
+      getTimes,
+      times,
+    }=this.props;
+    switch ( modal ) {
       case 'edit':
-        return <EditModal setCurrentModal={setCurrentModal} current={current} updateLibrary={updateLibraryRequest}libraries={libraries}/>
+        return <EditModal setCurrentModal={setCurrentModal} current={current} updateLibrary={updateLibraryRequest} libraries={libraries} />
       case 'add':
-        return <AddModal setCurrentModal={setCurrentModal} current={current} libraries={libraries} faculty={faculty} addLibrary={addLibraryRequest}/>
+        return <AddModal setCurrentModal={setCurrentModal} current={current} libraries={libraries} faculty={faculty} addLibrary={addLibraryRequest} />
       case 'addTimetable':
-        return <AddTimetableModal addTimetable={addTimetable} setCurrentModal={setCurrentModal} libraries={libraries} faculty={faculty} getTimes={getTimes} times={times}/>
+        return <AddTimetableModal addTimetable={addTimetable} setCurrentModal={setCurrentModal} libraries={libraries} faculty={faculty} getTimes={getTimes} times={times} />
       case 'editTimetable':
-        return <EditTimetableModal setCurrentModal={setCurrentModal} currentTimetable={currentTimetable} updateTimetable={updateTimetable}/>
+        return <EditTimetableModal setCurrentModal={setCurrentModal} currentTimetable={currentTimetable} updateTimetable={updateTimetable} />
       default:
         return false
     }
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps=state => ( {
   modal: state.modal.currentModal,
   current: state.libraries.current,
   libraries: state.libraries.list,
   faculty: state.login.user.faculty,
   currentTimetable: state.tables.currentTimetable,
   times: state.timetable.times,
-});
+} );
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
+const mapDispatchToProps=dispatch => {
+  return bindActionCreators( {
     setCurrentModal,
     updateLibraryRequest,
     addLibraryRequest,
     addTimetable,
     updateTimetable,
     getTimes,
-  }, dispatch);
+  }, dispatch );
 };
 
 
-const ModalWithStyle = CSSModules(Modal, styles);
-export default connect(mapStateToProps, mapDispatchToProps)(ModalWithStyle);
+const ModalWithStyle=CSSModules( Modal, styles );
+export default connect( mapStateToProps, mapDispatchToProps )( ModalWithStyle );
